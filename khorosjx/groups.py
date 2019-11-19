da1dd3563d0bd34d14c9f056b865b5a8c3b1549d
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2019-11-19 11:48:49 -0700

    Added missing import of khorosjx.core module

--- khorosjx/groups.py
+++ khorosjx/groups.py
@@ -6,9 +6,10 @@
 :Example:        Coming Soon
 :Created By:     Jeff Shurtliff
 :Last Modified:  Jeff Shurtliff
-:Modified Date:  17 Nov 2019
-:Version:        1.0.0
+:Modified Date:  19 Nov 2019
 """
+
+from . import core
 
 
 # Define function to verify the connection in the core module
