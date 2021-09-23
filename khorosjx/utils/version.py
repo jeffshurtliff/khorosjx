28713692bed950d2496dd6f51680910f4a3615d3
Jeff Shurtliff <jeff.shurtliff@rsa.com>
2021-09-22 20:54:50 -0600

    Updated version to 3.1.0 in version.py

--- khorosjx/utils/version.py
+++ khorosjx/utils/version.py
@@ -6,14 +6,14 @@
 :Example:           ``__version__ = version.get_full_version()``
 :Created By:        Jeff Shurtliff
 :Last Modified:     Jeff Shurtliff
-:Modified Date:     20 Sep 2021
+:Modified Date:     22 Sep 2021
 """
 
 import warnings
 
 import requests
 
-__version__ = "3.0.0"
+__version__ = "3.1.0"
 
 
 def get_full_version():
